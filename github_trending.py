669a11da4e9680da5ba0ff7c7ce1b31ca078ae85
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-05-26 20:57:58 +0300

    Added main script function

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,6 @@
-
-
-def get_trending_repositories(top_size):
+def main():
     pass
 
-
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
 
 if __name__ == '__main__':
-    pass
+    main()
