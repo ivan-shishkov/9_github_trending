1eae69e9de403d4b0de38cac1f0e8ec510920a14
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-05-30 21:19:35 +0300

    Simplified the getting of response of GET request

--- github_trending.py
+++ github_trending.py
@@ -5,10 +5,6 @@ import requests
 from requests.exceptions import ConnectionError
 
 
-class ResponseError(Exception):
-    pass
-
-
 def print_repository_issues_info(repository_issues_info):
     for issue_number, issue_info in enumerate(repository_issues_info, start=1):
         print('    Issue #{}'.format(issue_number))
@@ -47,33 +43,33 @@ def execute_get_request(url, params=None):
     try:
         response = requests.get(url, params=params)
     except ConnectionError:
-        raise ResponseError(
-            'Response not received. Check your Internet connection',
-        )
+        return None
 
-    if not response.ok:
-        raise ResponseError(
-            'Response status code is {}, should be 200'.format(
-                response.status_code,
-            ),
-        )
-    return response
+    return response.json() if response.ok else None
 
 
 def add_issues_info(repositories_info):
     for repository_info in repositories_info:
-        open_issues_info_response = execute_get_request(
+        open_issues_info = execute_get_request(
             url=repository_info['issues_url'].rstrip('{/number}'),
         )
-        repository_info['open_issues_info'] = open_issues_info_response.json()
+        if open_issues_info is None:
+            return False
+
+        repository_info['open_issues_info'] = open_issues_info
+
+    return True
 
 
 def get_repositories_info(url, url_params, count_repositories):
-    repositories_info_response = execute_get_request(
+    repositories_info = execute_get_request(
         url=url,
         params=url_params,
     )
-    return repositories_info_response.json()['items'][:count_repositories]
+    if repositories_info is None:
+        return None
+
+    return repositories_info['items'][:count_repositories]
 
 
 def main():
@@ -81,25 +77,23 @@ def main():
 
     print('Getting info about top starred repositories created last week...')
 
-    try:
-        repositories_info = get_repositories_info(
-            url='https://api.github.com/search/repositories',
-            url_params={
-                'q': 'created:>{}'.format(date.today() - timedelta(weeks=1)),
-                'sort': 'stars',
-                'order': 'decs',
-            },
-            count_repositories=count_repositories,
-        )
-    except ResponseError as error:
-        sys.exit(error)
+    repositories_info = get_repositories_info(
+        url='https://api.github.com/search/repositories',
+        url_params={
+            'q': 'created:>{}'.format(date.today() - timedelta(weeks=1)),
+            'sort': 'stars',
+            'order': 'decs',
+        },
+        count_repositories=count_repositories,
+    )
+
+    if repositories_info is None:
+        sys.exit('Could not get info about repositories')
 
     print('Getting info about open issues for every repository...')
 
-    try:
-        add_issues_info(repositories_info)
-    except ResponseError as error:
-        sys.exit(error)
+    if not add_issues_info(repositories_info):
+        sys.exit('Could not get info about open issues of repositories')
 
     print_repositories_info(
         repositories_info=repositories_info,
